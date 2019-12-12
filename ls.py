9797c33ab1ba382926cc57ab9fbfc1152a9e3b5f
spencercollins <spancergcollins@gmail.com>
2019-12-12 11:14:58 -0800

    stealling dakota's code as a base will change soon

--- ls.py
+++ ls.py
@@ -1,1 +1,11 @@
-def ls(String)
+def ls(index):
+    index = index + 1
+    openB = 0
+    closedB = 0
+    for i in range(findLocation(index), len(fileSystem)):
+        if fileSystem[i] == "[":
+            openB+=1
+        if fileSystem[i] == "]":
+            closedB +=1
+        if openB == closedB:
+            return fileSystem[findLocation(index):i+1]
