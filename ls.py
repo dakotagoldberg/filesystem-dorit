1e93c05ae48e632ca004565f75c6e9f33c978cf6
spencercollins <spancergcollins@gmail.com>
2019-12-14 14:18:27 -0800

    pushing some copied code so I can pull, that's lif

--- ls.py
+++ ls.py
@@ -1,7 +1,11 @@
+global fileSystem
+fileSystem = "[folder:dorit[folder:winter[folder:december[]][file:sleet][file:ice]]]"
+
 def ls(index):
     index = index + 1
     openB = 0
     closedB = 0
+    
     for i in range(findLocation(index), len(fileSystem)):
         if fileSystem[i] == "[":
             openB+=1
@@ -9,3 +13,16 @@ def ls(index):
             closedB +=1
         if openB == closedB:
             return fileSystem[findLocation(index):i+1]
+
+def findLocation(index):
+    openB = 0
+    closedB = 0
+    for i in range(len(fileSystem)):
+        if fileSystem[i] == "[":
+            openB+=1
+        if fileSystem[i] == "]":
+            closedB +=1
+        if openB == index + 1:
+            return i
+
+print(ls(0))
