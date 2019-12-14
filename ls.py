6a6f546fa1a613e0acbedc1a7b56f333b9d6a56c
spencercollins <spancergcollins@gmail.com>
2019-12-14 14:36:21 -0800

    Added Joshie's file and ls works... for now

--- ls.py
+++ ls.py
@@ -2,17 +2,32 @@ global fileSystem
 fileSystem = "[folder:dorit[folder:winter[folder:december[]][file:sleet][file:ice]]]"
 
 def ls(index):
+    print("working")
     index = index + 1
     openB = 0
     closedB = 0
-    
+    startRead = True
+    temp = ""
     for i in range(findLocation(index), len(fileSystem)):
         if fileSystem[i] == "[":
+            print ("open bracket")
             openB+=1
         if fileSystem[i] == "]":
+            print ("close bracket")
             closedB +=1
-        if openB == closedB:
-            return fileSystem[findLocation(index):i+1]
+            if startRead:
+                temp += " "
+        if openB == closedB +1:
+            print ("read")
+            startRead = True
+        elif True :
+            print ("close eyes")
+            startRead = False
+        if startRead and (fileSystem[i] != '[' and fileSystem[i] != ']'):
+            print (fileSystem[i])
+            temp += fileSystem[i]
+        if openB < closedB:
+            return temp
 
 def findLocation(index):
     openB = 0
@@ -25,4 +40,4 @@ def findLocation(index):
         if openB == index + 1:
             return i
 
-print(ls(0))
+print(ls(1))
