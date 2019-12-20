5f118d6f2ba97997dd73e515c78173e13a082f7c
LiamEdwards2 <56130395+LiamEdwards2@users.noreply.github.com>
2019-12-20 10:57:27 -0800

    Update createDirectoryProject.py

--- createDirectoryProject.py
+++ createDirectoryProject.py
@@ -1,18 +1,37 @@
-#ls #pwd #the String #name of the folder
-
-def mkdir(HowManyCharDeep, fileSystem, name):
-    folder = "[folder:" + str(name) + "[]]"
-    index = findPlace(pwd, fileSystem)
-    if index is None: 
-        return (1, "Can't do dat")
-    else: 
-        fileSystem = fileSystem[:index] + folder + fileSystem[index:]
-        return (0, fileSystem)
-
-def findPlace(HowManyCharDeep, fileSystem):
-
-
-    return HowManyCharDeep #find place will also find duplicat folders
-    return None #if there is a duplicate folder
-
+def mkdir(HowManyColDeep, fileSystem, name, input):
     
+    #make mkdir only take path
+    var = input.split("/") 
+    
+    if not var[0]:   
+        
+        
+        result = parsePath(var, name, HowManyColDeep)
+    else:
+        
+        var = currentPath(HowManyColDeep)[::-1] + var
+        
+        result = parsePath(var, name, HowManyColDeep)
+        
+        
+    if  result is not None:
+        folder = "folder:" + str(name) + "[]"
+        index = findLocation(HowManyColDeep)
+        fileSystem2 = fileSystem[:result] + folder + fileSystem[result:]
+        
+        return (0, fileSystem2) 
+    else:  
+        return (1, "Can't do dat")
+
+def parsePath(var, name, HowManyColDeep):
+    loc = HowManyColDeep
+    for i in range(len(var)-2): 
+        loc+=1
+        if not (var[i+1] in parseContents(reverseLookup(var[i]))) and var[0] is not None:
+            return None
+        
+        tempvar = parseContents(loc)
+    
+        if name in tempvar:
+            return None
+    return findLocation(reverseLookup(var[(len(var)-2)])) + len(var[(len(var)-2)]) + 2
