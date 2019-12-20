2f7fd2bf1c8bde7109f646b8b4e7e10fd3c4ace3
spencercollins <spancergcollins@gmail.com>
2019-12-20 08:26:24 -0800

    touch and also uploaded joshies wack edit

--- edit.py
+++ edit.py
@@ -1,17 +1,178 @@
-def edit(charNum, fileVal, newVal, fileNum):
+global fileSystem
+global myLoc
+fileSystem = "[folder:dorit[folder:winter[folder:december[folder:christmas[]][folder:kwanzaa[]]][folder:january[]][file:sleet is cool][file:ice]]]"
+myLoc = 1
 
- firstString = fileSystem[:charNum]
- secondString = fileSystem[charNum:]
- found = 0
- temp = 0
- if found != -1:
-  print "-1"
+
+def findLocation(index):
+    count = 0
+    for i in range(len(fileSystem)):
+        if fileSystem[i] == ":":
+            if (count == index):
+                return i
+            count+=1
+
+def reverseLookup(name):
+    count = 0
+    for i in range(len(fileSystem)):
+        if fileSystem[i] == ":":
+            if (currentFolderName(count) == name):
+                return count
+            count+=1
+
+
+def totalItems():
+    return fileSystem.count(":")
+
+
+def currentFolderContents(index):
+    openB = 0
+    closedB = 0
+    for i in range(findLocation(index) + len(currentFolderName(index))+1, len(fileSystem)):
+        if fileSystem[i] == "[":
+            openB+=1
+        if fileSystem[i] == "]":
+            closedB +=1
+        if openB == closedB - 1:
+            return fileSystem[findLocation(index) + len(currentFolderName(index))+1:i]
+
+def numberOfFilesInFolder(index):
+    count = 0
+    for i in parseContents(index):
+        if itemType(reverseLookup(i)) == "file":
+            count+=1
+    return count
+
+
+def currentFolderName(index):
+    i = findLocation(index)
+    while (fileSystem[i] != "[" and fileSystem[i] != "]"):
+        i+=1
+    return fileSystem[findLocation(index)+1:i]
+
+def itemType(index):
+    i = findLocation(index)
+    while (fileSystem[i] != "[" and fileSystem[i] != "]"):
+        i-=1
+    return fileSystem[i + 1:findLocation(index)]
+
+def currentPath(index):
+    path = []
+    path.append(currentFolderName(index))
+    openB = 0
+    closedB = 0
+    count = index
+    i = findLocation(index)
+    while (i > 0):
+        if fileSystem[i] == ":":
+            if (openB > closedB):
+                path.append(str(currentFolderName(count)))
+            count-=1
+        if fileSystem[i] == "[":
+            openB+=1
+        if fileSystem[i] == "]":
+            closedB +=1
+        i-=1
+    return path
+
+
+def parseContents(index):
+    raw = currentFolderContents(index)
+    items = []
+    openB = 0
+    closedB = 0
+    count = index + 1
+    start = findLocation(index) + len(currentFolderName(index)) + 1
+    i = start
+    while (i < len(raw) + start):
+        if fileSystem[i] == "[":
+            openB+=1
+        elif fileSystem[i] == "]":
+            closedB +=1
+        elif fileSystem[i] == ":":
+            if (openB - closedB == 1):
+                items.append(str(currentFolderName(count)))
+            count+=1
+        i+=1
+    return items
+
+
+def showHelp():
+    print("%5s %40s" % ("ls", "lists all items in folder")) 
+    print("%5s %40s" % ("cd", "changes directory")) 
+    print("%5s %40s" % ("mkdir", "creates a new directory")) 
+    print("%5s %40s" % ("touch", "creates a new file")) 
+    print("%5s %40s" % ("edit", "edits the contents of a file")) 
+    print("%5s %40s" % ("rm", "removes a file")) 
+    print("%5s %40s" % ("rmdir", "removes a folder"))  
+    print("%5s %40s" % ("path", "displays path of current location"))  
+
+
+def listContents(index):
+    numFiles = 0
+    for i in parseContents(index):
+        if itemType(reverseLookup(i)) == "file":
+            print(i + " --> " + itemType(reverseLookup(i)) + " ["+ str(numFiles) +"]")
+            numFiles+=1
+        else:
+            print(i + " --> " + itemType(reverseLookup(i)))
+
+def changeDirectory(currentIndex, newLoc):
+    if newLoc == ".." and currentIndex != 0:
+        x = currentPath(currentIndex)
+        return reverseLookup(x[1])
+    else:
+        x = parseContents(currentIndex)
+        if newLoc in x and itemType(reverseLookup(newLoc)) == "folder":
+            return reverseLookup(newLoc)
+    return currentIndex
+def edit(fileNum):
+
+ while True:
+  #print("while1")
+  newVal = input("Enter new value: ")
+
+  if newVal.isalnum():
+   #print("alpha")
+   break
+
+  else:
+
+   print ("Invalid value")
+ #print(parseContents(myLoc))
+ clown = numberOfFilesInFolder(myLoc)
+ #print(clown)
+ place = findLocation(myLoc)
+
+ if fileNum > clown:
+
+  return "-1"
+
  else:
-  for i = 0 to fileNum:
-   found = secondString.find(fileVal, (0+temp))
-   temp = secondString.find(fileVal, (0+temp))
-   temp+1
-  tempSub1 = secondString[:found]
-  tempSub2 = secondString[found:]
-  tempSub2.replace(fileVal, newVal)
-  fileSystem = firstString + tempSub1 + tempSub2
+
+  firstString = fileSystem[:place]
+
+  secondString = fileSystem[place:]
+
+  temp = 0
+
+  while fileNum !=0:
+   #print("while2")
+   num=secondString.find("file:",temp)
+
+   if num:
+
+    fileNum-=1
+
+    temp += num
+
+  subFind = secondString.find("]", temp)
+
+  tempSub1 = secondString[:(temp+5)]
+
+  tempSub2 = secondString[(subFind):]
+
+  return(tempSub1 + newVal + tempSub2)
+
+
+print(edit(2))
---
+++ touch.py
@@ -0,0 +1,150 @@
+
+global fileSystem
+global myLoc
+fileSystem = "[folder:dorit[folder:winter[folder:december[folder:christmas[]][folder:kwanzaa[]]][folder:january[]][file:sleet][file:ice]]]"
+myLoc = 2
+
+
+def findLocation(index):
+    count = 0
+    for i in range(len(fileSystem)):
+        if fileSystem[i] == ":":
+            if (count == index):
+                return i
+            count+=1
+
+def reverseLookup(name):
+    count = 0
+    for i in range(len(fileSystem)):
+        if fileSystem[i] == ":":
+            if (currentFolderName(count) == name):
+                return count
+            count+=1
+
+
+def totalItems():
+    return fileSystem.count(":")
+
+
+def currentFolderContents(index):
+    openB = 0
+    closedB = 0
+    for i in range(findLocation(index) + len(currentFolderName(index))+1, len(fileSystem)):
+        if fileSystem[i] == "[":
+            openB+=1
+        if fileSystem[i] == "]":
+            closedB +=1
+        if openB == closedB - 1:
+            return fileSystem[findLocation(index) + len(currentFolderName(index))+1:i]
+
+
+def currentFolderName(index):
+    i = findLocation(index)
+    while (fileSystem[i] != "[" and fileSystem[i] != "]"):
+        i+=1
+    return fileSystem[findLocation(index)+1:i]
+
+def itemType(index):
+    i = findLocation(index)
+    while (fileSystem[i] != "[" and fileSystem[i] != "]"):
+        i-=1
+    return fileSystem[i + 1:findLocation(index)]
+
+def currentPath(index):
+    path = []
+    path.append(currentFolderName(index))
+    openB = 0
+    closedB = 0
+    count = index
+    i = findLocation(index)
+    while (i > 0):
+        if fileSystem[i] == ":":
+            if (openB > closedB):
+                path.append(str(currentFolderName(count)))
+            count-=1
+        if fileSystem[i] == "[":
+            openB+=1
+        if fileSystem[i] == "]":
+            closedB +=1
+        i-=1
+    return path
+
+
+def parseContents(index):
+    raw = currentFolderContents(index)
+    items = []
+    openB = 0
+    closedB = 0
+    count = index + 1
+    start = findLocation(index) + len(currentFolderName(index)) + 1
+    i = start
+    while (i < len(raw) + start):
+        if fileSystem[i] == "[":
+            openB+=1
+        elif fileSystem[i] == "]":
+            closedB +=1
+        elif fileSystem[i] == ":":
+            if (openB - closedB == 1):
+                items.append(str(currentFolderName(count)))
+            count+=1
+        i+=1
+    return items
+
+def listContents(index):
+    for i in parseContents(index):
+        print(i + " --> " + itemType(reverseLookup(i)))
+
+def changeDirectory(currentIndex, newLoc):
+    if newLoc == ".." and currentIndex != 0:
+        x = currentPath(currentIndex)
+        return reverseLookup(x[1])
+    else:
+        x = parseContents(currentIndex)
+        if newLoc in x and itemType(reverseLookup(newLoc)) == "folder":
+            return reverseLookup(newLoc)
+    return currentIndex
+
+def createPath(input):
+    input = input[1:len(input)] + "/"
+    temp = ""
+    numSlash = 0
+    for i in range(len(input)):
+        if(input[i] == '/'):
+            numSlash+=1
+    output = []
+    numSlash = 0
+    #print(output)
+    for i in range(len(input)):
+        if(input[i] == '/'):
+            #print(temp)
+            #print(output)
+            output += [temp]
+            temp = ""
+            numSlash += 1
+        else:
+            temp += "" + input[i]
+    return output
+
+def touch1(input):
+    tempLoc = 0
+    place = createPath(input)
+    for i in place:
+        print(tempLoc)
+        if tempLoc == None:
+            print("error input not supported")
+        tempLoc = changeDirectory(tempLoc, i)
+    startPoint = findLocation(tempLoc)
+    if(fileSystem[startPoint+ len(currentFolderName(tempLoc))+1:startPoint+ len(currentFolderName(tempLoc))+3] == "[]"
+    
+    Triforce1 = fileSystem[0:startPoint + len(currentFolderName(tempLoc))+1]
+    #print(Triforce1)
+    Triforce2 = "[file:]"
+    Triforce3 = fileSystem[startPoint+ len(currentFolderName(tempLoc))+1:len(fileSystem)]
+    #print(Triforce3)
+    #print(Triforce1+Triforce2+Triforce3)
+    
+    return Triforce1 +Triforce2 + Triforce3
+        
+
+
+print(touch1(input("hey")))
